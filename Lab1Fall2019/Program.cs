6b065b29a324345caa5ccd99315ef38bcbec4539
Dawn Mitchell <S00027159@acad.tri-c.edu>
2019-10-01 20:57:19 -0400

    Add project files.

---
+++ Lab1Fall2019/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Lab1Fall2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+
+            
+            
+            int heightFeet;
+            double heightInches;
+
+            Console.Write("What is your first name?: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine();
+            
+            Console.Write("What is your middle initial?: ");
+            middleInitial = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("What is your last name?: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine();
+
+
+            fullName = firstName + " " + middleInitial + ". " + lastName;
+            Console.Write("Well, hello... ");
+            Console.WriteLine(fullName);
+            Console.WriteLine("");
+
+           
+
+            Console.Write("What if your height in feet?: ");
+            heightFeet = int.Parse(Console.ReadLine());
+
+            Console.Write("And how many inches?: ");
+            heightInches = double.Parse(Console.ReadLine());
+
+            double inches = heightFeet * 12;
+            inches += heightInches;
+
+            double heightCM = 2.54 * inches;
+
+            int age;
+            bool isCitizen;
+
+            Console.Write("Your age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Citizen?: ");
+            bool isCitizen = Console.ReadLine().ToLower().StartsWith("y");
+
+            Console.WriteLine(age >= 18 && isCitizen);
+                       
+            Console.ReadKey();
+        }
+    }
+}
